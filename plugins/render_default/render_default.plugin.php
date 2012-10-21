0ecbd700eaf9e9ff0d8ef174a490682ed1741f55
Kaspars <kaspars@konstruktors.com>
2012-10-21 23:30:58 +0200

    Remove trailing and init slash

--- plugins/render_default/render_default.plugin.php
+++ plugins/render_default/render_default.plugin.php
@@ -27,7 +27,7 @@ class render_default extends orbiter {
 		else
 			$article['filename'] = basename( $article['filename'] );
 
-		$article['permalink'] = ltrim( $article['uri'], '/' ) . '/' . $article['slug'];
+		$article['permalink'] = ltrim( $article['uri'] . '/' . $article['slug'], '/' );
 
 		return $article;
 	}
