d8f31ce50606fa537179f2deee0c3c3780123092
Kaspars <kaspars@konstruktors.com>
2012-10-21 22:47:55 +0200

    Make it all simple and beautiful

--- publish.php
+++ publish.php
@@ -1,218 +1,138 @@
 <?php
 
-/**
- * Load and verify config
- */
-
-$config_path = __DIR__ . DIRECTORY_SEPARATOR . 'config.ini'; 
-
-if ( file_exists( $config_path ) )
-	$config = parse_ini_file( $config_path );
-else
-	die( 'config.ini could not be found.' );
-
-if ( empty( $config ) )
-	die( 'config.ini is empty.' );
-
-$config_req = array( 'docs', 'template', 'public' );
-
-if ( array_intersect( $config_req, array_keys( $config ) ) !== $config_req )
-	die( 'All required configuration settings are not specified.' );
+new orbiter();
 
 
-/**
- * Includes
- */
+class orbiter {
 
-// Mustache
-include( 'lib/mustache/src/Mustache/Autoloader.php' );
-Mustache_Autoloader::register();
-$config['mustache_engine'] = new Mustache_Engine;
+	public $docs = array();
+	public $articles = array();
 
-// Markdown
-include( 'lib/php-markdown-1.0.1o/markdown.php' );
-
-// Typography
-include( 'lib/php-typography/php-typography.php' );
-$config['typography'] = new phpTypography();
+	static public $template = array();
+	static public $config = array();
+	static public $filters = array();
 
 
-/**
- * Collect all articles and parse the content
- */
+	function orbiter() {
+		
+		$this->load_plugins();
+		
+		foreach ( $this->load_config() as self::$config ) {
 
-$files = glob_docs( '*.md', $config['docs'] );
+			// Load the template
+			$this->load_template();
 
-/**
- * Generate articles
- */
-
-if ( ! empty( $files ) )
-	$articles = parse_docs( $files );
-else
-	report_error( 'No source files found.' );
-
-
-/**
- * Generate HTML files
- */
-
-if ( isset( $articles ) && ! empty( $articles ) )
-	generate_html( $articles );
-else
-	report_error( 'No HTML files were generated because no articles were found.' );
-
-/**
- * Return errors, if any
- */
-
-show_errors();
-
-/**
- * Functions
- */
-
-function glob_docs( $pattern, $path ) {
-	$files = glob( $path . $pattern );
-	$dirs = glob( $path . '*', GLOB_ONLYDIR | GLOB_NOSORT );
-
-	if ( ! empty( $dirs ) )
-		foreach ( $dirs as $dir )
-			$files = array_merge( $files, glob( $dir . DIRECTORY_SEPARATOR . $pattern ) );
-	
-	return $files;
-}
-
-function parse_docs( $files ) {
-	global $config;
-
-	$articles = array();
-
-	foreach ( $files as $file ) {
-
-		// Don't publish drafts
-		if ( strstr( $file, '.draft') )
-			continue;
-
-		// Create the slug
-		$p = pathinfo( $file );
-		$base = basename( $p['dirname'] );
-		$slug = $p['filename'];
-
-		// Setup the article
-		$article = array(
-				'file' => $file,
-				'slug' => $slug,
-				'content' => file_get_contents( $file ),
-				'filemtime' => filemtime( $file ),
-				'title' => ''
-			);
-
-		// Extract meta data
-		if ( $meta = get_meta( $article['content'] ) ) {
-			$article = array_merge( $article, $meta['meta'] );
-			$article['content'] = $meta['content'];
+			// Render the all output files
+			$this->parse_render();
+		
 		}
 
-		// Apply Markdown parser, generate HTML
-		$article['content'] = Markdown( $article['content'] );
+	}
+
+
+	private function load_config() {
+
+		if ( file_exists( __DIR__ . '/config.ini' ) )
+			return parse_ini_file( __DIR__ . '/config.ini', true );
+		else if ( file_exists( __DIR__ . '/config_sample.ini' ) )
+			return parse_ini_file( __DIR__ . '/config_sample.ini', true );
+		else
+			die( 'Config could not be loaded.' );
+
+	}
+
+
+	private function load_template() {
+
+		$tempate_files = $this->glob_files( '*', realpath( self::$config['template'] ) );
+
+		foreach ( $tempate_files as $template_file )
+			self::$template[ basename( $template_file ) ] = file_get_contents( $template_file );
+
+	}
+
+
+	private function parse_render() {
+
+		$docs = $this->glob_files( self::$config['docs_extension'], realpath( self::$config['docs'] ) );
+
+		foreach ( $docs as $file ) {
+
+			// Setup the article
+			$article = array(
+					'file' => $file,
+					'content' => file_get_contents( $file ),
+					'filemtime' => filemtime( $file ),
+					'id' => md5( $file ),
+					'config' => self::$config
+				);
+
+			// Append article to the list
+			$this->articles[] = $this->filter( 'parse_document', $article );
+
+		}
+
+		// Render index pages
+		$this->filter( 'render_index', $this->articles );
+
+		// Render each article
+		foreach ( $this->articles as $article )
+			$this->filter( 'render_article', $article, $this->articles );
+
+	}
+
+
+	private function load_plugins() {
+
+		// Find all plugins
+		foreach ( $this->glob_files( '*.plugin.php', __DIR__ . '/plugins' ) as $plugin )
+			include( $plugin );
+
+		// Autoload plugins
+		foreach ( get_declared_classes() as $class )
+			if ( is_subclass_of( $class, get_class( $this ) ) )
+				new $class();
+
+	}
+
+
+	public function glob_files( $pattern, $path ) {
+
+		$files = glob( $path . '/' . $pattern );
+		$dirs = glob( $path . '/*', GLOB_ONLYDIR );
+
+		if ( ! empty( $dirs ) )
+			foreach ( $dirs as $dir )
+				$files = array_merge( $files, $this->glob_files( $pattern, $dir ) );
 		
-		// Apply typography
-		$article['content'] = $config['typography']->process( $article['content'] );
+		return $files;
 
-		// Extract a default title if none set
-		if ( empty( $article['title'] ) )
-			if ( preg_match('|<h[^>]+>(.*)</h[^>]+>|iU', $article['content'], $heading ) )
-				$article['title'] = $heading[1];
+	}	
 
-		// Append article to the list
-		$articles[] = $article;
+
+	static public function add_filter( $hook, $callback, $weight = 10 ) {
+
+		// Register a filter
+		self::$filters[ $hook ][ $weight ][] = $callback;
+
+		// Sort filters by their weights
+		ksort( self::$filters[ $hook ] );
+
 	}
 
-	return $articles;
-}
 
-function generate_html( $articles = array() ) {
-	global $config;
+	static public function filter( $hook, $value, $args = array() ) {
 
-	if ( empty( $articles ) )
-		return false;
+		if ( ! isset( self::$filters[ $hook ] ) )
+			return $value;
 
-	$template = file_get_contents( $config['template'] );
+		foreach ( self::$filters[ $hook ] as $weight )
+			foreach ( $weight as $callback )
+				$value = call_user_func( $callback, $value, $args );
 
-	// Generated individual articles
-	foreach ( $articles as $article ) {
-		$path = $config['public'] . $article['slug'] . '/';
+		return $value;
 
-		if ( ! is_dir( $path ) )
-			mkdir( $path );
-
-		// Add images folder as a symlink
-		if ( is_dir( dirname( $article['file'] ) . '/images' ) && ! file_exists( $path . 'images' ) )
-			symlink( dirname( $article['file'] ) . '/images', $path . 'images' );
-
-		file_put_contents( 
-			$path . 'index.html', 
-			$config['mustache_engine']->render( 
-				$template, 
-				array(
-					'article' => $article 
-				)
-			)
-		);
 	}
 
-	// Generate the index page
-	file_put_contents( 
-		$config['public'] . 'index.html', 
-		$config['mustache_engine']->render( 
-			$template, 
-			array(
-				'index' => array_values( $articles )
-			)
-		) 
-	);
-}
-
-function get_meta( $text ) {
-
-	// Meta must be seperated with two new lines
-	$split_meta = explode( "\n\n", $text, 2 );
-	
-	if ( count( $split_meta ) !== 2 )
-		return false;
-
-	// Each meta entry is separated by a single line break
-	$meta_rows = explode( "\n", current( $split_meta ) . "\n", -1 );
-	
-	if ( empty( $meta_rows ) )
-		return false;
-
-	$meta = array();
-
-	foreach ( $meta_rows as $row )
-		if ( $maybe_meta_row = explode( ':', trim( $row ), 2 ) )
-			if ( count( $maybe_meta_row ) == 2 )
-				$meta['meta'][ trim( $maybe_meta_row[0] ) ] = trim( $maybe_meta_row[1] );
-
-	if ( isset( $meta['meta'] ) )
-		$meta['content'] = end( $split_meta );
-
-	return $meta;
-}
-
-function report_error( $error ) {
-	global $errors;
-
-	$errors[] = $error;
-}
-
-function show_errors() {
-	global $errors;
-
-	if ( ! isset( $errors ) || empty( $errors ) )
-		return;
-
-	echo json_encode( $errors );
 }
 
