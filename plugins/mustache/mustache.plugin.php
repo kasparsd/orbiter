7b4fce48ebe3fb375f5c03dd719a50c534b0b29c
Kaspars <kaspars@konstruktors.com>
2012-10-21 22:46:33 +0200

    Add Mustache support

---
+++ plugins/mustache/mustache.plugin.php
@@ -0,0 +1,22 @@
+<?php
+
+
+class mustache extends orbiter {
+
+	private $m;
+	
+	function mustache() {
+
+		orbiter::add_filter( 'render_article_html', array( $this, 'render_article_html' ) );
+
+		include( 'mustache/src/Mustache/Autoloader.php' );
+		Mustache_Autoloader::register();
+		$this->m = new Mustache_Engine;
+	}
+	
+	function render_article_html( $article, $template ) {
+
+		return $this->m->render( $template, $article );
+		
+	}
+}
