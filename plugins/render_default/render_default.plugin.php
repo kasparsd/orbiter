4e91c4be51e3c82c253443cba24896b8b15bd40a
Kaspars <kaspars@konstruktors.com>
2012-10-23 22:43:04 +0200

    Make this a document resolver, instead of renderer

--- plugins/render_default/render_default.plugin.php
+++ plugins/render_default/render_default.plugin.php
@@ -4,72 +4,32 @@
 class render_default extends orbiter {
 
 	function render_default() {
-		orbiter::add_filter( 'parse_document', array( $this, 'render_path_setup' ), 50 );
-		orbiter::add_filter( 'render_article', array( $this, 'render_article' ), 50 );
+		orbiter::add_filter( 'parse_document', array( $this, 'article_setup' ), 50 );
+		orbiter::add_filter( 'render', array( $this, 'render_view' ), 50 );
 	}
 
-	function render_path_setup( $article ) {
-		
-		$slug_path = str_replace( realpath( orbiter::$config['docs'] ), '', dirname( $article['file'] ) );
-		$slug = array_shift( explode( '.', basename( $article['file'] ) ) );
+	function article_setup( $article ) {
 
-		// Add a default slug
-		if ( ! isset( $article['slug'] ) )
-			$article['slug'] = $slug;
-
-		// Add a default location
-		if ( ! isset( $article['uri'] ) )
-			$article['uri'] = $slug_path;
-
-		// We know what we're doing, so don't put it in a sub-folder
-		if ( isset( $article['filename'] ) )
-			$article['slug'] = dirname( $slug_path );
-
-		// Specify a default template file
-		if ( ! isset( $article['template'] ) )
-			$article['template'] = 'template.html';
-
-		// Specify a default destination filename
-		if ( ! isset( $article['filename'] ) )
-			$article['filename'] = 'index.html';
-		else
-			$article['filename'] = basename( $article['filename'] );
-
-		// Create a permalink
+		// Add permalink
 		$article['permalink'] = ltrim( $article['uri'] . '/' . $article['slug'], '/' );
 
 		return $article;
 	}
 
+	function render_view( $articles ) {
 
-	function render_article( $article, $articles ) {
+		// Extract request path
+		$request_uri = str_replace( dirname( $_SERVER['SCRIPT_NAME'] ), '', $_SERVER['REQUEST_URI'] );
 
-		$destination = realpath( orbiter::$config['public'] ) . '/' . $article['permalink'] . '/' . $article['filename'];
+		// Remove query args and opening/trailing slashes
+		$request_uri = trim( array_shift( explode( '?', $request_uri ) ) , '/' );
 
-		// Create path to that folder
-		if ( ! is_dir( dirname( $destination ) ) )
-			mkdir( dirname( $destination ), 0777, true );
+		// Remove index.php
+		$request_uri = str_replace( basename( $_SERVER['SCRIPT_NAME'] ), '', $request_uri );
 
-		// Automatically symlink the assets folder
-		if ( is_dir( dirname( $article['file'] ) . '/assets' ) && ! is_link( dirname( $destination ) . '/assets' ) )
-			symlink( dirname( $article['file'] ) . '/assets', dirname( $destination ) . '/assets' );
-
-		$template_vars = array( 
-				'article' => $article, 
-				'articles' => $articles, 
-				'config' => orbiter::$config 
-			);
-
-		$template_vars = orbiter::filter( 'render_article_vars', $template_vars, $article, $articles );
-
-		if ( isset( orbiter::$template[ $article['template'] ] ) )
-			file_put_contents( 
-					orbiter::filter( 'render_article_destination', $destination, $article ),
-					orbiter::filter( 'render_article_html', $template_vars, orbiter::$template[ $article['template'] ] ) 
-				);
-
-		return $article;
-
+		foreach ( $articles as $article )
+			if ( $article['permalink'] == $request_uri )
+				die( orbiter::filter( 'render_article_html', array( 'article' => $article, 'articles' => $articles, 'config' => orbiter::$config ), orbiter::$template[ $article['template'] ] ) );
 	}
 
 }
