67d460469a0d45333b19c161b7f2b1fbbfabb4eb
Kaspars <kaspars@konstruktors.com>
2012-10-24 11:43:45 +0200

    we are not using public_root here anymore

--- plugins/parser_helpers/parser_helpers.plugin.php
+++ plugins/parser_helpers/parser_helpers.plugin.php
@@ -9,7 +9,7 @@ class parser_helpers extends orbiter {
 
 	function img_to_relative( $article ) {
 
-		if ( isset( orbiter::$config['home'] ) && isset( orbiter::$config['public_root'] ) ) {
+		if ( isset( orbiter::$config['home'] ) ) {
 			$base = orbiter::$config['home'] . '/' . trim( orbiter::$config['docs'], '/\\' ) . str_replace( realpath( orbiter::$config['docs'] ), '', dirname( $article['file'] ) );
 			$article['content'] = preg_replace( '/src="([^http:|\/][^\"]+)"/i', sprintf( 'src="%s/$1"', $base ), $article['content'] );
 		}
