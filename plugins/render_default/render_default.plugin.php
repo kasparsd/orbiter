7fa56793d6e129ae8db6a4c955728d7ea5fbdf26
Kaspars <kaspars@konstruktors.com>
2012-10-25 16:46:37 +0200

    Send proper headers for RSS feeds

--- plugins/render_default/render_default.plugin.php
+++ plugins/render_default/render_default.plugin.php
@@ -3,10 +3,20 @@
 
 class render_default extends orbiter {
 
+
 	function render_default() {
+		
+		// Generate a permalink
 		orbiter::add_filter( 'parse_document', array( $this, 'article_setup' ), 50 );
+
+		// Parse the request and resolve which file to use
 		orbiter::add_filter( 'render', array( $this, 'render_view' ), 50 );
+
+		// Add default headers for things like RSS feeds
+		orbiter::add_filter( 'render_article_html', array( $this, 'http_headers' ), 5 );
+
 	}
+
 
 	function article_setup( $article ) {
 
@@ -14,7 +24,9 @@ class render_default extends orbiter {
 		$article['permalink'] = trim( $article['uri'] . '/' . $article['slug'], '/' );
 
 		return $article;
+
 	}
+
 
 	function render_view( $articles ) {
 
@@ -29,15 +41,33 @@ class render_default extends orbiter {
 
 		foreach ( $articles as $article )
 			if ( $article['permalink'] == $request_uri )
-				die( orbiter::filter( 'render_article_html', array( 'article' => $article, 'articles' => $articles, 'config' => orbiter::$config ), orbiter::$template[ $article['template'] ], $request_uri ) );
-		
+				die( orbiter::filter( 'render_article_html', array( 'article' => $article, 'articles' => $articles, 'config' => orbiter::$config ), $request_uri ) );
+
+		// Render Not Found
 		header('HTTP/1.0 404 Not Found');
 
 		foreach ( $articles as $article )
 			if ( $article['permalink'] == '404' )
-				die( orbiter::filter( 'render_article_html', array( 'article' => $article, 'articles' => $articles, 'config' => orbiter::$config ), orbiter::$template[ $article['template'] ], $request_uri ) );
+				die( orbiter::filter( 'render_article_html', array( 'article' => $article, 'articles' => $articles, 'config' => orbiter::$config ), $request_uri ) );
 
-		die( orbiter::filter( 'render_article_html', array( 'article' => array( 'content' => 'Page Not Found' ), 'articles' => $articles, 'config' => orbiter::$config ), orbiter::$template[ 'template.html' ], $request_uri ) );
+		die( orbiter::filter( 'render_article_html', array( 'article' => array( 'content' => 'Page Not Found' ), 'articles' => $articles, 'config' => orbiter::$config ), $request_uri ) );
+	
 	}
+
+
+	function http_headers( $content ) {
+
+		if ( ! isset( $content['article']['template'] ) )
+			return $content;
+
+		$path = pathinfo( $content['article']['template'] );
+
+		if ( $path['extension'] == 'xml' )
+			header( 'Content-type: text/xml; charset=UTF-8' );
+
+		return $content;
+
+	}
+
 
 }
