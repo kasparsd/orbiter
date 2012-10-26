9877f8cc4cebd3cb1ed7fe7f868365fdd3255d8e
Kaspars <kaspars@konstruktors.com>
2012-10-26 11:26:43 +0200

    Add support for template partials

--- plugins/mustache/mustache.plugin.php
+++ plugins/mustache/mustache.plugin.php
@@ -9,16 +9,16 @@ class mustache extends orbiter {
 
 		include( 'mustache/src/Mustache/Autoloader.php' );
 		Mustache_Autoloader::register();
-
-		$this->m = new Mustache_Engine(
-				orbiter::filter( 'mustache_engine', array() )
-			);
+		
+		$this->m = new Mustache_Engine( orbiter::filter( 'mustache_engine_init', array() ) );
 
 		orbiter::add_filter( 'render_article_html', array( $this, 'render_article_html' ) );
 		
 	}
 	
 	function render_article_html( $vars ) {
+
+		$this->m->setPartials( orbiter::$template );
 		
 		if ( isset( $vars['article']['template'] ) )
 			return $this->m->render( orbiter::$template[ $vars['article']['template'] ], $vars );
