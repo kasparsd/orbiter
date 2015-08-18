fdcee18d33124f8b861913fb9d2ed016c50038bd
Pilskalns <andzs@pilskalns.lv>
2015-08-18 18:22:26 +0300

    Complete issue #2 fix
    Minus fast forward to nextissue.

--- plugins/meta/meta.plugin.php
+++ plugins/meta/meta.plugin.php
@@ -16,7 +16,7 @@ class meta extends orbiter_plugin {
 			$article['content'] = file_get_contents( $article['file'] );
 
 		// Meta must be seperated with two new lines
-		$ini = explode( PHP_EOL.PHP_EOL, $article['content'], 2 );
+		$ini = explode( "\n\n", $article['content'], 2 );
 		
 		if ( count( $ini ) !== 2 )
 			return $article;
